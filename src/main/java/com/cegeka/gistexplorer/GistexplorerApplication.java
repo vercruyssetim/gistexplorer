3ac15106d440800faf4fd7f1f39b0cbe6ec239af
vercruyssetim <tim.vercruysse@cegeka.be>
2019-08-21 11:04:58 +0200

    Added logging

--- src/main/java/com/cegeka/gistexplorer/GistexplorerApplication.java
+++ src/main/java/com/cegeka/gistexplorer/GistexplorerApplication.java
@@ -1,6 +1,8 @@
 package com.cegeka.gistexplorer;
 
 import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class GistexplorerApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GistexplorerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(GistexplorerApplication.class, args);
     }
 
     @Bean
     public RestTemplate restTemplate() {
+        LOGGER.info(System.getenv("GITHUB_TOKEN"));
+        LOGGER.info(System.getProperty("GITHUB_TOKEN"));
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setInterceptors(Lists.newArrayList((request, body, execution) -> {
             request.getHeaders().add("Authorization", System.getenv("GITHUB_TOKEN"));
